a15cacea4c462b5e134f5e91d5541079523879ca
Andrei Justyn Sesbreno <asesbreno441@gmail.com>
2025-10-11 18:45:33 +0800

    Enable course

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import mongoose from 'mongoose';
-
+import cors from 'cors'
 
 import {userRoutes} from './routes/user.js'
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use ('/api/users', userRoutes)
 
